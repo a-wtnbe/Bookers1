bb1409beac51dd8dc03b2f13b8c83aebed8a98c9
EC2 Default User <ec2-user@ip-172-31-35-27.ec2.internal>
2021-04-15 23:59:33 +0000

    bookモデルの中が変えあって 空ってエラー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,39 @@
 class BooksController < ApplicationController
-  def edit
+  def index
+    @book = Book.all
+    @book = Book.new
   end
 
-  def index
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to new_book_path
+  end
+
+  # こっから下はコントローラーの中でしか呼ばないぞ
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
